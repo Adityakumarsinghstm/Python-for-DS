208b47180614b4a24fc1cbcd8e8f344ee95bfb38
Aditya <adityasingh62065@gmail.com>
2024-05-07 00:36:12 +0530

    Input and output in another file week 6 complete

---
+++ mypack/mymath.py
@@ -0,0 +1,12 @@
+''' This is a custom module '''
+print("\n Welcome to mymath module")
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mult(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+def mod(x,y):
+    return x%y
---
+++ mypack/mystr.py
@@ -0,0 +1,9 @@
+''' This module contains basic operations on string '''
+print('Welcome to mystr module \n')
+def str_len(s):
+    return len(s)
+def str_cmp(s1,s2):
+    return s1 == s2 
+def str_cat(s1,s2):
+    return s1 + s2
+    
